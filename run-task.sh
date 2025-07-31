958cd3eaaa16f7b793362b83cb915d0edb919a24
Maksimenko_Mikhail <367371@niuitmo.ru>
2025-07-31 19:42:42 +0300

    decrease max error count

--- run-task.sh
+++ run-task.sh
@@ -6,6 +6,6 @@
 ./build/task-exec RM_64_22.gen trellis_based_rmld_results/ResGV_RM_64_22.txt 50 1 1 0
 ./build/task-exec RM_64_42.gen trellis_based_rmld_results/ResGV_RM_64_42.txt 50 1 1 0
 ./build/task-exec RM_64_57.gen trellis_based_rmld_results/ResGV_RM_64_57.txt 50 1 1 0
-./build/task-exec RM_64_22.gen trellis_based_rmld_results/ResGU_RM_64_22.txt 500 1 1 1
-./build/task-exec RM_64_42.gen trellis_based_rmld_results/ResGU_RM_64_42.txt 500 1 1 1
-./build/task-exec RM_64_57.gen trellis_based_rmld_results/ResGU_RM_64_57.txt 500 1 1 1
+./build/task-exec RM_64_22.gen trellis_based_rmld_results/ResGU_RM_64_22.txt 100 1 1 1
+./build/task-exec RM_64_42.gen trellis_based_rmld_results/ResGU_RM_64_42.txt 100 1 1 1
+./build/task-exec RM_64_57.gen trellis_based_rmld_results/ResGU_RM_64_57.txt 100 1 1 1
